98a40f871bb7f58fb6b0f011e9885bd0613a6789
nezahabjan <nezahabjan123@gmail.com>
2018-12-06 23:14:40 +0100

    dodana funkcija OX

--- projektOR.py
+++ projektOR.py
@@ -63,6 +63,27 @@ def OX(stars1, stars2, a, b):
     otrok1[a, b] = stars1[a, b]
     otrok2[a, b] = stars2[a, b]
     
+def OX(stars1, stars2, a,b):
+	otrok1=[0]*len(stars1)
+	otrok2=[0]*len(stars2)
+	otrok1[a:b]=stars1[a:b]
+	otrok2[a:b]=stars2[a:b]
+	vmesni1=otrok1[b:len(otrok1)] + otrok1[0:b]
+	vmesni2=otrok2[b:len(otrok2)] + otrok2[0:b]
+	s1=0
+	s2=0
+	for i in chain(range(b, len(stars2)-1), range(b)):
+		if stars2[i] not in vmesni1:
+			vmesni1[s1]=stars2[i]
+			s1=s1+1
+	for i in chain(range(b, len(stars1)-1), range(b)):
+		if stars1[i] not in vmesni2:
+			vmesni2[s2]=stars1[i]
+			s2=s2+1
+	print([vmesni1, vmesni2])
+	pravi1=vmesni1[a-1:len(vmesni1)] + vmesni1[0:a-1]
+	pravi2=vmesni2[a-1:len(vmesni2)] + vmesni2[0:a-1]
+	return([pravi1, pravi2])
     
     
     
