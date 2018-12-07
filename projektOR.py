fa50404b7f6e4081693ee4a606f451daa984d002
Gasper Letnar <gasper.letnar@gmail.com>
2018-12-07 17:33:28 +0100

    Ustvarjeni fji potomci in ga_tsp. Pri prvi ustvarimo iz trenutne populacije naslednjo generacijo. Druga fja se ni dokoncana, je pa to nasa glavna fja. Testirano na manjsem primeru, z vecanjem stevila ponovitev se zdi da algoritem vrne krajso pot.
    Signed-off-by: Gasper Letnar <gasper.letnar@gmail.com>

--- projektOR.py
+++ projektOR.py
@@ -8,8 +8,6 @@ def utezi(n, maxCena):
     utezi = np.random.randint(0, maxCena, size=(n, n))
     np.fill_diagonal(utezi, 0)
     return(utezi)
-    
-cene = utezi(5, 100)
 
 # Izracunamo dolzino dane poti iz cen povezav v matriki utezi.
 
@@ -19,10 +17,6 @@ def dolzinaPoti(pot, utezi):
         dolzina += utezi.item((pot[i] - 1, pot[i+1] - 1))
     dolzina += utezi.item((pot[len(pot)-1] - 1, pot[0] - 1))
     return(dolzina)
-
-pot = list(range(1, len(cene) + 1))
-random.shuffle(pot)   
-dolzina = dolzinaPoti(pot, cene)
 
 # Nakljucno ustvari zacetno populacijo velikosti popVelikost. Posamezen predstavnik populacije je neka pot med vsemi vozlisci.
 # V slovarju imamo vsako pot podano v paru z njeno dolzino. popVelikost izberemo sami.
@@ -38,28 +32,25 @@ def populacija(popVelikost, utezi):
         
     return(poti)
 
-poti = populacija(10, cene)
-
 # Selekcija s turnirjem. Druga opcija bi bila proporcionalna selekcija.
 # Iz populacije izberemo nakljucno k kromosomov/elementov/poti. Funkcija vrne par zmagovalec - pot z najkrajso dolzino - in njeno dolzino.
 # Število kromosomov(poti) v turnirju - k -  izberemo sami. Potrebno pazljivo izbrati k. Vecji kot je, hitrejsa je konvergenca (to ni nujno dobro).
 
-def turnir(pop, k):
-    vozlisca = list(range(1, len(pop) + 1))
+# Spremenil, da fja vrne samo pot, ne pa par (pot, dolzina). Ce se spreminjamo nazaj, je potrebno spremeniti kodo tudi v fji potomoci. Gasper
+
+def turnir(populacija, kTurnir):
+    vozlisca = list(range(1, len(populacija) + 1))
     random.shuffle(vozlisca)
-    izbranci = vozlisca[:k]
+    izbranci = vozlisca[:kTurnir]
     minimum = math.inf
     pot = []
-    print(izbranci)
     
     for i in izbranci:
-        if pop[i][1] < minimum:
-            minimum = pop[i][1]
-            pot = pop[i][0]
+        if populacija[i][1] < minimum:
+            minimum = populacija[i][1]
+            pot = populacija[i][0]
             
-    return([pot, minimum])
-
-stars = turnir(poti, 4)
+    return(pot)
 
 # Ordered crossover(OX). Ustvarimo dva potomoca iz dveh starsev z "ordered crossover" metodo križanja.
 # a <= b <= len(pot). Stars je neka pot, ki je zmagala na turnirju.
@@ -70,30 +61,22 @@ stars = turnir(poti, 4)
 # v vrstnem redu 3. podzaporedje, 1. podazporedje, 2. podzaporedje. Seveda izkljucimo tista vozlisca, ki so ze vsebovana. Ko pridemo do konca,
 # nadaljujemo se od zacetka do a-tega mesta. Enako za drugega potomca, le da vlogi starsev zamenjamo.
 
-def ox(stars1, stars2, a, b):
+def ox(stars1, stars2, rez_a, rez_b):
     dolzina = len(stars1)
     
-    ostanek1 = stars2[b:] + stars2[:b]
-    for vozl in stars1[a:b]:
+    ostanek1 = stars2[rez_b:] + stars2[:rez_b]
+    for vozl in stars1[rez_a:rez_b]:
         ostanek1.remove(vozl)
-    otrok1 = ostanek1[dolzina - b:] + stars1[a:b] + ostanek1[:dolzina - b]
+    otrok1 = ostanek1[dolzina - rez_b:] + stars1[rez_a:rez_b] + ostanek1[:dolzina - rez_b]
     
-    ostanek2 = stars1[b:] + stars1[:b]
-    for vozl in stars2[a:b]:
+    ostanek2 = stars1[rez_b:] + stars1[:rez_b]
+    for vozl in stars2[rez_a:rez_b]:
         ostanek2.remove(vozl)
-    otrok2 = ostanek2[dolzina - b:] + stars2[a:b] + ostanek2[:dolzina - b]
+    otrok2 = ostanek2[dolzina - rez_b:] + stars2[rez_a:rez_b] + ostanek2[:dolzina - rez_b]
     
     return(otrok1, otrok2)
-            
-s1 = list(range(8))
-s2 = list(range(8))
-random.shuffle(s1)
-random.shuffle(s2)
-a = 3
-b = 5
-otroka = ox(s1, s2, a, b)
 
-# Vsako vozlisce z verj verjMutacije mutiramo - zamenjamo polozaj mutiranega vozlisca in nakljucnega vozlisca na poti.
+# Vsako vozlisce z verjetnostjo verjMutacije mutiramo - zamenjamo polozaj mutiranega vozlisca in nakljucnega vozlisca na poti.
 # S tem ohranjamo raznolikost populacije in se poskusamo izogniti prehitri konvergenci, ki nas lahko vodi blizu lokalnega, ne pa globalnega optimuma.
 
 def mutacija(otrok, verjMutacije):
@@ -102,16 +85,49 @@ def mutacija(otrok, verjMutacije):
             j = random.randint(0, len(otrok) - 1)
             otrok[i], otrok[j] = otrok[j], otrok[i]
     return(otrok)
-            
-ot = otroka[0]
-mut = mutacija(ot.copy(), 0.05)    
-    
+
+# Ustvarimo naslednjo generacijo tako, da izbiramo po 2 starsa iz populacije, ki ju nato krizamo, da dobimo 2 potomca.
+# Oba potomca nato mutiramo. Ponavljamo dokler ni mnozica potomcev enako velika kot populacija.
+
 # Potrebno se je odlociti ali ze izbranega starsa vrnemo v populacijo iz katere izbiramo v novem turnirju ali ne.
 # Z drugimi besedami: ali nas moti, da je kak stars izbran veckrat?
- 
-    
-    
-    
-    
+# Prav tako moramo razmisliti ali bomo upostevali "elitism" - da se dolocen delez najkrajsih poti v populaciji avtomaticno prenese naprej.
 
+def potomci(utezi, populacija, verjMutacije, rez_a, rez_b, kTurnir):
+    potomci = {}
+    for i in range(int(len(populacija)/2)):
+        stars1 = turnir(populacija, kTurnir)
+        stars2 = turnir(populacija, kTurnir)
+        
+        otroka = ox(stars1, stars2, rez_a, rez_b)
+        otrok1 = mutacija(otroka[0], verjMutacije)
+        otrok2 = mutacija(otroka[1], verjMutacije)
+        
+        potomci[2*i+1] = otrok1, dolzinaPoti(otrok1, utezi)
+        potomci[2*(i+1)] = otrok2, dolzinaPoti(otrok2, utezi)
+    return(potomci)
 
+# Genetski algoritem na problemu potujocega trgovca
+
+# Trenutno fja naredi fiksno v naprej doloceno stevilo ponovitev izbiranje potomcev.
+# Potrebno razmisliti tudi o kakem drugem zaustavitvenem kriteriju.
+
+def ga_tsp(ponovitve, utezi, populacija, verjMutacije, rez_a, rez_b, kTurnir):
+    nasledniki = populacija
+    for _ in range(ponovitve):
+        nasledniki = potomci(utezi, nasledniki, verjMutacije, rez_a, rez_b, kTurnir)
+        
+    "potrebno dopisati se kodo, da vrne najkrajso pot, trenutno vrne celotno zadnjo generacijo"
+    
+    return(nasledniki)
+
+##########################################################################################################################################
+
+cene = utezi(5, 100)
+poti = populacija(10, cene)
+k = 4
+a= 3
+b = 5
+mut = 0.02
+p = ga_tsp(1000 , cene, poti, mut, a, b, k)
+  
